444e205808c7c88cab905547a0c1546a255190b0
wakumaku <wakumaku@gmail.com>
2025-01-06 00:04:22 +0100

    Adds examples

---
+++ examples/send_message/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/wakumaku/go-zulip"
+	"github.com/wakumaku/go-zulip/messages"
+)
+
+func main() {
+	ctx := context.Background()
+
+	// Initialize client
+	c, err := zulip.NewClient("https://chat.zulip.org", "email@zulip.org", "0123456789")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Send a message to a channel/topic
+	msgSvc := messages.NewService(c)
+	sendMessageResponse, err := msgSvc.SendMessage(ctx,
+		messages.ToChannelTopic(messages.ToChannelName("general"), "greetings"),
+		"Hello Zulip!",
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Fetch the previous message
+	fetchMessageResponse, err := msgSvc.FetchSingleMessage(ctx, sendMessageResponse.ID)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(fetchMessageResponse.Message.ID, fetchMessageResponse.Message.Content)
+}
