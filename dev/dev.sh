1fe92960889c85da4200bd1b80a4a69d45145a5a
Jordi Tarruella <jordi.tarruella@zoundream.com>
2025-01-09 18:03:52 +0100

    removes hyphen in docker-compose command

--- dev/dev.sh
+++ dev/dev.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-docker-compose -p go-zulip \
+docker compose -p go-zulip \
 -f docker-compose-dev-env.yml \
 -f docker-compose-dev.yml \
 -f docker-compose-zulip.yml \
 up
 
-docker-compose -p go-zulip \
+docker compose -p go-zulip \
 down
