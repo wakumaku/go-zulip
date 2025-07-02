4e950b46a8dec9d48f9685b6fb5d986d3532ffcb
wakumaku <wakumaku@gmail.com>
2025-07-02 23:47:08 +0200

    fix realtime/delete_message test

--- realtime/events/delete_message_test.go
+++ realtime/events/delete_message_test.go
@@ -11,13 +11,14 @@ import (
 func TestDeleteMessage(t *testing.T) {
 	eventExample := `{
     "id": 0,
+	"type": "delete_message",
     "message_id": 58,
     "message_ids": [
         58,
         57
     ],
     "stream_id": 5,
-    "topic": "new_topic",
+    "topic": "new_topic"
 }`
 
 	v := events.DeleteMessage{}
@@ -28,7 +29,7 @@ func TestDeleteMessage(t *testing.T) {
 	assert.Equal(t, events.DeleteMessageType, v.EventType())
 	assert.Equal(t, "delete_message", v.EventOp())
 
-	assert.Equal(t, 58, v.MessageID)
+	assert.Equal(t, 58, *v.MessageID)
 	assert.Equal(t, 2, len(v.MessageIDs))
 	assert.Equal(t, 5, *v.StreamID)
 	assert.Equal(t, "new_topic", *v.Topic)
