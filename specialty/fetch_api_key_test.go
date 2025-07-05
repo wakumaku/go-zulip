0e758e68d368eaa53948356104524a46e784f4e2
wakumaku <wakumaku@gmail.com>
2025-07-05 02:35:54 +0200

    adds tests

---
+++ specialty/fetch_api_key_test.go
@@ -0,0 +1,52 @@
+package specialty_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/alecthomas/assert/v2"
+	"github.com/wakumaku/go-zulip/specialty"
+)
+
+func TestFetchAPIKey(t *testing.T) {
+	client := createMockClient(`{
+    "api_key": "gjA04ZYcqXKalvYMA8OeXSfzUOLrtbZv",
+    "email": "iago@zulip.com",
+    "msg": "",
+    "result": "success",
+    "user_id": 5
+}`)
+
+	service := specialty.NewService(client)
+
+	resp, err := service.FetchAPIKeyProduction(context.Background(), "iago@zulip.com", "password")
+	assert.NoError(t, err)
+	assert.Equal(t, "success", resp.Result())
+	assert.Equal(t, "gjA04ZYcqXKalvYMA8OeXSfzUOLrtbZv", resp.APIKey)
+
+	// validate the parameters sent are correct
+	assert.Equal(t, map[string]any{
+		"username": "iago@zulip.com",
+		"password": "password",
+	}, client.(*mockClient).paramsSent)
+}
+
+func TestFetchAPIKeyDevelopment(t *testing.T) {
+	client := createMockClient(`{
+    "api_key": "gjA04ZYcqXKalvYMA8OeXSfzUOLrtbZv",
+    "email": "iago@zulip.com",
+    "msg": "",
+    "result": "success",
+    "user_id": 5
+}`)
+	service := specialty.NewService(client)
+	resp, err := service.FetchAPIKeyDevelopment(context.Background(), "iago@zulip.com")
+	assert.NoError(t, err)
+	assert.Equal(t, "success", resp.Result())
+	assert.Equal(t, "gjA04ZYcqXKalvYMA8OeXSfzUOLrtbZv", resp.APIKey)
+
+	// validate the parameters sent are correct
+	assert.Equal(t, map[string]any{
+		"username": "iago@zulip.com",
+	}, client.(*mockClient).paramsSent)
+}
