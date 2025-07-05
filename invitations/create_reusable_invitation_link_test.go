b7223f0f0aeb0999b14507b0d5752de79c5226cd
wakumaku <wakumaku@gmail.com>
2025-07-05 02:40:33 +0200

    Adds tests

---
+++ invitations/create_reusable_invitation_link_test.go
@@ -0,0 +1,29 @@
+package invitations_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/wakumaku/go-zulip/invitations"
+)
+
+func TestCreateReusableInvitationLink(t *testing.T) {
+	client := createMockClient(`{
+    "invite_link": "https://example.zulipchat.com/join/yddhtzk4jgl7rsmazc5fyyyy/",
+    "msg": "",
+    "result": "success"
+}`)
+
+	service := invitations.NewService(client)
+
+	resp, err := service.CreateReusableInvitationLink(context.Background(), invitations.StreamIds([]int{1, 2}))
+	assert.NoError(t, err)
+	assert.Equal(t, "success", resp.Result())
+	assert.Equal(t, "https://example.zulipchat.com/join/yddhtzk4jgl7rsmazc5fyyyy/", resp.InviteLink)
+
+	// validate the parameters sent are correct
+	assert.Equal(t, map[string]any{
+		"stream_ids": "[1,2]",
+	}, client.(*mockClient).paramsSent)
+}
---
+++ invitations/invitations_test.go
@@ -0,0 +1,34 @@
+package invitations_test
+
+import (
+	"context"
+	"encoding/json"
+	"io"
+
+	"github.com/wakumaku/go-zulip"
+)
+
+// mockClient is a mock implementation of zulip.RESTClient
+type mockClient struct {
+	response   string
+	method     string
+	path       string
+	paramsSent map[string]any
+}
+
+func (mc *mockClient) DoRequest(ctx context.Context, method, path string, data map[string]any, response zulip.APIResponse, opts ...zulip.DoRequestOption) error {
+	mc.method = method
+	mc.path = path
+	mc.paramsSent = data
+	return json.Unmarshal([]byte(mc.response), response)
+}
+
+func (mc *mockClient) DoFileRequest(ctx context.Context, method, path string, fileName string, file io.Reader, response zulip.APIResponse, opts ...zulip.DoRequestOption) error {
+	return nil
+}
+
+func createMockClient(response string) zulip.RESTClient {
+	return &mockClient{
+		response: response,
+	}
+}
